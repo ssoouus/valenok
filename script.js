639034071d87dcadab8db2726d41780378d7505d
ssoouus <glebobrezkov@yandex.ru>
2025-02-09 15:32:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,13 @@
-const canvas = document.getElementById("fireworks");
-const ctx = canvas.getContext("2d");
+document.addEventListener("click", (event) => {
+    const photoContainer = document.getElementById("photo-container");
+    const img = document.createElement("img");
+    img.src = "photo.jpg"; 
+    img.className = "photo";
+    img.style.left = event.clientX + "px";
+    img.style.top = event.clientY + "px";
+    photoContainer.appendChild(img);
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const particles = [];
-
-class Particle {
-    constructor(x, y, size, color, speedX, speedY) {
-        this.x = x;
-        this.y = y;
-        this.size = size;
-        this.color = color;
-        this.speedX = speedX;
-        this.speedY = speedY;
-        this.life = 100;
-    }
-
-    update() {
-        this.x += this.speedX;
-        this.y += this.speedY;
-        this.life -= 2;
-    }
-
-    draw() {
-        ctx.globalAlpha = this.life / 100;
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        ctx.fill();
-    }
-}
-
-function createParticles(x, y) {
-    const colors = ["#ff6b6b", "#ff9f43", "#feca57", "#48dbfb", "#1dd1a1"];
-    for (let i = 0; i < 50; i++) {
-        const size = Math.random() * 4 + 1;
-        const color = colors[Math.floor(Math.random() * colors.length)];
-        const speedX = (Math.random() - 0.5) * 4;
-        const speedY = (Math.random() - 0.5) * 4;
-        particles.push(new Particle(x, y, size, color, speedX, speedY));
-    }
-}
-
-canvas.addEventListener("click", (event) => {
-    createParticles(event.clientX, event.clientY);
-});
-
-function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < particles.length; i++) {
-        particles[i].update();
-        particles[i].draw();
-        if (particles[i].life <= 0) {
-            particles.splice(i, 1);
-            i--;
-        }
-    }
-    requestAnimationFrame(animate);
-}
-
-animate();
-
-window.addEventListener("resize", () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    setTimeout(() => {
+        img.remove();
+    }, 2000);
 });
